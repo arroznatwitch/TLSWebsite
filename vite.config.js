eee94d48d2f9019a77c46e47609c1c570c1612c0
Cardoso <rui.pedro.goncalves.cardoso@gmail.com>
2026-06-17 22:46:30 +0100

    add deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/thelastsurvivor/',
+  base: '/thelastsurvivor/', // Deploying to GitHub Pages, set the base path to the repository name
 })
