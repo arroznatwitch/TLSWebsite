923db268644542792189effab4279fc4c9d52ebd
Cardoso <rui.pedro.goncalves.cardoso@gmail.com>
2026-06-25 21:51:56 +0100

    Now works with the domain

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/thelastsurvivor/', // Deploying to GitHub Pages, set the base path to the repository name
+  base: '/', // Domínio próprio (playthelastsurvivor.com) serve na raiz
 })
